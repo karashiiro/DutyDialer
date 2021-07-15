2cfeb942ab939e623a3ade9fcb8c993a42a12169
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-07-15 12:56:31 -0700

    feat(app): improve address entry view layout

--- app/lib/server_address_entry_view.dart
+++ app/lib/server_address_entry_view.dart
@@ -1,7 +1,7 @@
 import 'package:duty_dialer/web_socket_stream.dart';
 import 'package:flutter/material.dart';
 
-class ServerAddressEntryView extends StatelessWidget {
+class ServerAddressEntryView extends StatefulWidget {
   const ServerAddressEntryView(
       {Key? key,
       required this.streamSocket,
@@ -14,31 +14,49 @@ class ServerAddressEntryView extends StatelessWidget {
   final void Function() onConnectButtonPressed;
 
   @override
+  _ServerAddressEntryViewState createState() => _ServerAddressEntryViewState();
+}
+
+class _ServerAddressEntryViewState extends State<ServerAddressEntryView> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Text(
-            'Plugin server address',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-          ),
-          TextField(
-            onChanged: onAddressFieldChanged,
+            'DutyDialer',
+            style: TextStyle(fontSize: 48, fontWeight: FontWeight.w600),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 30),
+            padding:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height / 6),
           ),
-          ElevatedButton(
-            onPressed: onConnectButtonPressed,
-            style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size(120, 45)),
-              textStyle: MaterialStateProperty.all(TextStyle(
-                fontSize: 18,
-              )),
+          Center(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width - 168,
+                  child: TextField(
+                    onChanged: widget.onAddressFieldChanged,
+                    decoration:
+                        InputDecoration(hintText: 'Plugin server address'),
+                  ),
+                ),
+                Spacer(),
+                ElevatedButton(
+                  onPressed: widget.onConnectButtonPressed,
+                  style: ButtonStyle(
+                    fixedSize: MaterialStateProperty.all(Size(120, 45)),
+                    textStyle: MaterialStateProperty.all(TextStyle(
+                      fontSize: 18,
+                    )),
+                  ),
+                  child: Text('Connect'),
+                ),
+              ],
             ),
-            child: Text('Connect'),
-          )
+          ),
         ],
       ),
     );
